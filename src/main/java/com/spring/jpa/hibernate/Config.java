ee2af4e1aa27404a2a2dd42ec40599e553185647
Sandeep <sandeepds2002@gmail.com>
2019-04-11 07:41:59 +0530

    Initial Commit
    Initial Commit

---
+++ src/main/java/com/spring/jpa/hibernate/Config.java
@@ -0,0 +1,11 @@
+package com.spring.jpa.hibernate;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = "com.spring.jpa.hibernate.Resource")
+@Configuration
+public class Config {
+
+
+}
---
+++ src/main/java/com/spring/jpa/hibernate/SpringDataJpaHibernateExampleApplication.java
@@ -0,0 +1,12 @@
+package com.spring.jpa.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaHibernateExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaHibernateExampleApplication.class, args);
+	}
+}
