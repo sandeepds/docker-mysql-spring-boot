f6be1364abdef468531ac89cf8bbdb13e93e3636
Sandeep <sandeepds2002@gmail.com>
2019-04-12 12:22:09 +0530

    Update UserRepository.java
    Updated UserRepository

--- src/main/java/com/spring/jpa/hibernate/resource/UserRepository.java
+++ src/main/java/com/spring/jpa/hibernate/resource/UserRepository.java
@@ -1,6 +1,8 @@
 package com.spring.jpa.hibernate.resource;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("usersRepository")
 public interface UserRepository extends JpaRepository <Users, Integer> {
 }
